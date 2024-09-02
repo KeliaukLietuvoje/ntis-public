5262dbb52b1e63ffe1f3f2cd91337b5b2c9b7b50
Petras Pauliūnas <petras.pauliunas@gmail.com>
2024-09-02 11:15:32 +0300

    fix prefix

--- html/wpapp/themes/ntis/inc/shortcodes/map/map.php
+++ html/wpapp/themes/ntis/inc/shortcodes/map/map.php
@@ -6,9 +6,9 @@ class NTIS_Map
 {
     public function __construct()
     {
-        add_shortcode('tip_map', array($this, 'tip_map_shortcode'));
+        add_shortcode('ntis_map', array($this, 'ntis_map_shortcode'));
     }
-    public function tip_map_shortcode($atts)
+    public function ntis_map_shortcode($atts)
     {
         $atts = shortcode_atts(
             array(
@@ -20,19 +20,19 @@ class NTIS_Map
                 'add_layer' => 'false'
             ),
             $atts,
-            'tip_map'
+            'ntis_map'
         );
 
         $maplibre_css_ver = $maplibre_js_ver = '4.3.2';
         wp_enqueue_style('maplibre-styles', '//unpkg.com/maplibre-gl@'.$maplibre_js_ver.'/dist/maplibre-gl.css', [], $maplibre_js_ver, 'all');
         wp_enqueue_script('maplibre-js', '//unpkg.com/maplibre-gl@'.$maplibre_css_ver.'/dist/maplibre-gl.js', ['jquery'], $maplibre_css_ver, true);
-        $tip_map_js_ver  = date("ymd-His", filemtime(NTIS_THEME_DIR . '/inc/shortcodes/map/map.js'));
-        $tip_map_css_ver  = date("ymd-His", filemtime(NTIS_THEME_DIR . '/inc/shortcodes/map/map.css'));
-        wp_enqueue_style('ntis-map-styles', NTIS_THEME_URL . '/inc/shortcodes/map/map.css', [], $tip_map_css_ver, 'all');
-        wp_enqueue_script('ntis-map-js', NTIS_THEME_URL . '/inc/shortcodes/map/map.js', ['jquery'], $tip_map_js_ver, true);
+        $ntis_map_js_ver  = date("ymd-His", filemtime(NTIS_THEME_DIR . '/inc/shortcodes/map/map.js'));
+        $ntis_map_css_ver  = date("ymd-His", filemtime(NTIS_THEME_DIR . '/inc/shortcodes/map/map.css'));
+        wp_enqueue_style('ntis-map-styles', NTIS_THEME_URL . '/inc/shortcodes/map/map.css', [], $ntis_map_css_ver, 'all');
+        wp_enqueue_script('ntis-map-js', NTIS_THEME_URL . '/inc/shortcodes/map/map.js', ['jquery'], $ntis_map_js_ver, true);
 
 
-        wp_localize_script('ntis-map-js', 'tip_map_config', array(
+        wp_localize_script('ntis-map-js', 'ntis_map_config', array(
             'api' => ['url' => NTIS_API_URL],
             'coordinates' => explode(',', $atts['coordinates']),
             'zoom' => (int)$atts['zoom'],
